64508f3d186ff37e77e2b62dce930dc781b41af6
GOWTHAMI9999 <72733925+GOWTHAMI9999@users.noreply.github.com>
2026-03-17 14:11:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,34 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Security Group (SSH allow)
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "example" {
-  ami           = "ami-0ddda618e961f2270"  # ✅ Amazon Linux 2 (us-east-2)
-  instance_type = "t3.micro"
+  ami           = "ami-0198cdf7458a7a932"
+  instance_type = "t2.micro"
+
+  key_name        = "saikey"
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
 }
